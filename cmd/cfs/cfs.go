c6afafc263d74fa287595316e09c72fb65bc3b0f
Kousuke Kawahira <kawahira@binzume.net>
2019-11-15 04:40:33 +0900

    cmd dir

--- cmd/cfs/cfs.go
+++ cmd/cfs/cfs.go
@@ -9,9 +9,9 @@ import (
 
 	"github.com/gorilla/websocket"
 
-	"../fuse"
-	"../volume"
-	"../ws_volume"
+	"../../fuse"
+	"../../volume"
+	"../../ws_volume"
 )
 
 var defaultHubAPI = "http://localhost:8080"
